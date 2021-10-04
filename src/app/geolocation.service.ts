7b82aa797b916faa03ef4afd4a336fcfb9714738
Patrick McDonald <764290+WhatsThatItsPat@users.noreply.github.com>
2021-10-04 11:24:57 -0400

    Show coordinates in display

--- src/app/geolocation.service.ts
+++ src/app/geolocation.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { Geolocation } from '@capacitor/geolocation';
 
 interface Coords {
   latitude: number | null;
@@ -18,5 +19,29 @@ export class GeolocationService {
   // eslint-disable-next-line @typescript-eslint/member-ordering
   public coords$: Observable<Coords> = this.coordsSubject;
 
-  constructor() { }
+  constructor() {
+    this.start();
+  }
+
+  async start() {
+
+    const watchOptions = {
+      // enableHighAccuracy: false,
+      // timeout: 10000,
+      // maximumAge: 0
+    };
+
+    const watchId = await Geolocation.watchPosition(
+      watchOptions,
+      (position, error) => {
+        console.log({position, error});
+
+        const { coords: {latitude, longitude } } = position;
+
+        this.coordsSubject.next({latitude, longitude});
+      }
+    );
+
+    console.log({ watchId });
+  }
 }
